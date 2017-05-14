ebcd12b45181523d15218ffec2868c03ecd034ce
Joseph Kim <josephkim0224@gmail.com>
2017-05-14 11:41:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return nestedTarget = document.getElementById('nested').getElementsByClassName('target')[0];
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list');
+  for (var i = 0; i < rankedLists.length; i++) {
+    rankedLists[i].innerHTML = parseInt(rankedLists[i]) + n;
+  }
+}
+
+function deepestChild() {
+  // var current = document.getElementById('grand-node');
+  var current = document.querySelector('#grand-node');
+  var previous;
+
+  while (current) {
+    if (current.childElementCount > 0) {
+      current = current.children[0];
+    } else {
+      return current.parentNode;
+    }
+  }
+}
+
+deepestChild();
